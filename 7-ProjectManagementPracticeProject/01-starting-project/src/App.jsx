00e4395882fcd7dc174e7f032efdedab3d15e7b4
AndrewSushi <AndrewSushi8@gmail.com>
2023-12-20 22:22:48 -0800

    Added New Project saving functionality. and also added funcitoning buttons

--- 7-ProjectManagementPracticeProject/01-starting-project/src/App.jsx
+++ 7-ProjectManagementPracticeProject/01-starting-project/src/App.jsx
@@ -1,11 +1,61 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  function handleCancelNewProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      }
+    })
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        projects: [...projectsState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState)
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject} onCancelNewProject={handleCancelNewProject}/>
+  }else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
