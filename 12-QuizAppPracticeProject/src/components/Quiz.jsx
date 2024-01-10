9276cbb547d84d5a43c289221b55c5bf14c040ed
AndrewSushi <andrewsush8i@gmail.com>
2024-01-09 18:44:56 -0800

    Fixed timer and question bugs

--- 12-QuizAppPracticeProject/src/components/Quiz.jsx
+++ 12-QuizAppPracticeProject/src/components/Quiz.jsx
@@ -5,34 +5,18 @@ import Question from './Question.jsx';
 import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz(){
-  const [answerState, setAnswerState] = useState('')
   const [userAnswers, setUserAnswers] = useState([])
 
-  const activeQuestionIndex = 
-    answerState === '' ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ){
-    setAnswerState('answered')
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnswer]
     })
-
-    setTimeout(() => {
-      if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
-        setAnswerState("correct")
-      }else{
-        selectedAnswer('wrong')
-      }
-
-      setTimeout(() => {
-        setAnswerState('')
-      }, 2000)
-    }, 1000)
-  }, 
-  [activeQuestionIndex])
+  }, [])
 
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
@@ -49,10 +33,7 @@ export default function Quiz(){
     <div id="quiz">
       <Question 
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text} 
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
         onSkipAnswer={handleSkipAnswer}
       />
